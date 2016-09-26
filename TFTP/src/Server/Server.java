5a40bab00ec23b80e2d43972294c799586a888c2
Guelor Emanuel <ncstech07@gmail.com>
2016-09-26 03:08:01 -0400

    Added connection thread and updated server

--- TFTP/src/Server/Server.java
+++ TFTP/src/Server/Server.java
@@ -1,5 +1,63 @@
 package Server;
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.*; 
+import java.net.*;
+import java.util.*;
+
 
 public class Server {
+	//TODO:  Set port
+	public final static int DEFAULT_PORT = 69;
+    private static final int SERVERPORT = DEFAULT_PORT; //FOR NOW
+    byte[] buffer = new byte[512];
 
+    private boolean running = false;
+	
+	
+
+    public void run() {
+
+        running = true;
+        
+
+        try {
+            System.out.println("S: Connecting...");
+
+            DatagramSocket serverSocket = new DatagramSocket(SERVERPORT);
+            ArrayList<Connection> connections = new ArrayList<>();
+
+            while(running){
+            
+                DatagramPacket packet =  new DatagramPacket(buffer, buffer.length );
+                serverSocket.receive(packet);
+                System.out.println("SERVER: Accepted connection.");
+                System.out.println("SERVER: received"+new String(packet.getData(), 0, packet.getLength()));
+               
+                //new socket created with random port for thread
+                DatagramSocket threadSocket = new DatagramSocket();
+                
+                Connection clientConnection = new Connection(threadSocket, packet, connections);
+                connections.add(clientConnection);
+                clientConnection.start();
+            } //end while
+            
+
+        } catch (Exception e) {
+            System.out.println("S: Error");
+            e.printStackTrace();
+        }
+
+      
+    }
+	
+	
+	public static void main(String[] args) {
+		//Create server and start it. 
+		Server server = new Server();
+		System.out.println("STARTING SERVER ON PORT: " + SERVERPORT);
+		server.run();
+	}
+
+	
 }
