aa01e7db3d917469095b8db1f37fc34663c50ef1
Guelor Emanuel <ncstech07@gmail.com>
2016-09-28 15:29:21 -0400

    Added TFTPTransferHandler to anything to do with rcv and sending files

--- TFTP/src/Utilities/TFTPAbortException.java
+++ TFTP/src/Utilities/TFTPAbortException.java
@@ -1,7 +1,11 @@
 package Utilities;
 
 public class TFTPAbortException extends Exception {
-	private static final long serialVersionUID = -2879110457073551679L;
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7184563335328451077L;
 
 	public TFTPAbortException(String message) {
 		super(message);
